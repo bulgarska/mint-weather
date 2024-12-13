d44af1b7765755853efdd8fb2b24fdd09d2125fb
Milen Mintchev <171292500+bulgarska@users.noreply.github.com>
2024-12-12 17:21:25 -0800

    Updated the ui using customtkinter. Icon images are no longer saved prior to being displayed.

--- mint_weather.py
+++ mint_weather.py
@@ -1,11 +1,14 @@
+from io import BytesIO
+import os
 import requests
-import tkinter as tk
-from tkinter import *
+import customtkinter
 from tkinter import messagebox
-from PIL import Image, ImageTk
+from PIL import Image
 import datetime
 
 
+# Currently displays the time for sunrise and sunset 
+# of any location relative to your current timezone
 def epoch_to_date_time(epoch):
     date_time = datetime.datetime.fromtimestamp(epoch)
     return date_time.strftime("%H:%M")
@@ -14,7 +17,7 @@ def epoch_to_date_time(epoch):
 def get_weather_data():
     city_name = city_entry.get()
     # Use your openweathermap api key here
-    api_key = 'Insert_API_key'
+    api_key = os.environ.get('open_weather_map_api_key')
     open_weather_map_base_url = 'http://api.openweathermap.org/data/2.5/weather?'
     complete_url = f'{open_weather_map_base_url}q={city_name}&appid={api_key}&units=metric'
 
@@ -26,8 +29,8 @@ def display_weather_data():
     weather_data = get_weather_data()
 
     # Show loading state
-    weather_label.config(text="Loading...")
-    icon_label.config(image='')
+    weather_label.configure(text="Loading...")
+    icon_label.configure(image='')
 
     if weather_data['cod'] == 200:
         main_data = weather_data['main']
@@ -42,7 +45,7 @@ def display_weather_data():
         wind_speed = weather_data['wind']['speed']
         visibility = weather_data['visibility']
 
-        weather_label.config(text=f'Temperature: {temperature:.0f}°C '
+        weather_label.configure(text=f'Temperature: {temperature:.0f}°C '
                                   f'\nSunrise: {epoch_to_date_time(sunrise_time)} '
                                   f'\nSunset: {epoch_to_date_time(sunset_time)} '
                                   f'\nDescription: {weather_description} '
@@ -58,42 +61,46 @@ def display_weather_data():
     icon_name = weather_data['weather'][0]['icon']
     icon_url = f"http://openweathermap.org/img/wn/{icon_name}@2x.png"
     icon_data = requests.get(icon_url)
-    with open("icon.png", "wb") as f:
-        f.write(icon_data.content)
-    icon_image = ImageTk.PhotoImage(Image.open("icon.png"))
-    icon_label.config(image=icon_image)
-    icon_label.image = icon_image
+    # with open("icon.png", "wb") as f:
+    #     f.write(icon_data.content)
+    icon_image = customtkinter.CTkImage(dark_image=Image.open(BytesIO(icon_data.content)), 
+                                        size=(100, 100))
+    icon_label.configure(image=icon_image)
+    # icon_label.image = icon_image
 
+customtkinter.set_appearance_mode('dark')
+customtkinter.set_default_color_theme('green')
 
-root = tk.Tk()
-root.geometry('800x600')
+root = customtkinter.CTk()
+root.geometry('1000x700')
 root.title('Mint Weather')
-root.config(bg='#ffc0cb')
 
 # City entry frame
-city_entry_frame = tk.Frame(root, bg='#cbffc0', bd=5)
-city_entry_frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
+city_entry_frame = customtkinter.CTkFrame(master=root, )
+city_entry_frame.pack(pady=20, padx=60, fill ='both', expand=True)
 
 # City entry text box
-city_entry = tk.Entry(city_entry_frame, bg='#ffc0cb', font=("Courier", 14))
-city_entry.place(relwidth=0.65, relheight=1)
-city_entry.insert(0, "Los Angeles")
+city_entry = customtkinter.CTkEntry(master=city_entry_frame, placeholder_text='Los Angeles', 
+                                    font=("Courier", 14))
+city_entry.pack(pady=12, padx=10)
 
 # Get weather button
-get_weather_button = tk.Button(city_entry_frame, bg='#ffc0cb', text="Get Weather", font=("Courier", 12),
-                               command=display_weather_data)
-get_weather_button.place(relx=0.7, relwidth=0.3, relheight=1)
+get_weather_button = customtkinter.CTkButton(master=city_entry_frame, text="Get Weather", 
+                               font=("Courier", 12), command=display_weather_data)
+get_weather_button.pack(pady=12, padx=10)
 
 # Weather label frame
-weather_frame = tk.Frame(root, bg='#cbffc0', bd=10)
-weather_frame.place(relx=0.5, rely=0.3, relwidth=0.75, relheight=0.5, anchor='n')
-weather_label = tk.Label(weather_frame, bg='#ffc0cb', font=("Courier", 14), justify='left', bd=5, anchor='w')
-weather_label.place(relwidth=1, relheight=1)
+weather_frame = customtkinter.CTkFrame(master=root, )
+weather_frame.pack(pady=20, padx=60, fill ='both', expand=True)
+weather_label = customtkinter.CTkLabel(master=weather_frame, 
+                                       text='', 
+                                       font=("Courier", 14))
+weather_label.pack(pady=12, padx=10)
 
 # Weather icon frame
-icon_frame = tk.Frame(weather_frame, bg='#cbffc0')
-icon_label = tk.Label(icon_frame, bg='#ffc0cb')
-icon_label.place(relx=0.5, rely=0.5, anchor='center')
-icon_frame.place(relx=0.8, rely=0.3, relwidth=0.2, relheight=0.4, anchor='n')
+icon_frame = customtkinter.CTkFrame(master=weather_frame)
+icon_label = customtkinter.CTkLabel(master=icon_frame, image='', text='')
+icon_label.pack(pady=12, padx=10)
+icon_frame.pack(pady=20, padx=60, expand=True)
 
 root.mainloop()
